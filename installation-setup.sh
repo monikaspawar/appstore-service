98c72d6baeee23ce68b920b390b46642814852e3
Ubuntu <ubuntu@ip-172-31-34-172.ap-south-1.compute.internal>
2025-11-23 18:32:14 +0000

    new file added installtion.sh

---
+++ installation-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+sudo apt update -y
+sudo apt install openjdk-17-jdk -y
+sudo apt install maven -y
+sudo apt install git -y
+sudo apt install openssh-client -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install jenkins -y
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+sudo -u jenkins ssh-keygen -t rsa -b 4096 -C "jenkins" -f /var/lib/jenkins/.ssh/id_rsa -N ""
+sudo cat /var/lib/jenkins/.ssh/id_rsa.pub
+
